d2003d071c77ce4dce0bc06c497cf4e228dc09a4
Svetlin86 <93228248+Svetlin86@users.noreply.github.com>
2022-01-31 11:31:55 +0200

    Create Conditions 8.java

---
+++ Conditions 8.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int input = scanner.nextInt();
+
+        if(input < 100){
+            System.out.println("Less than 100");
+        }
+        if(input >= 100 && input <= 200){
+            System.out.println("Between 100 and 200");
+        }
+        if(input > 200){
+            System.out.println("Greater than 200");
+        }
+    }
+}
