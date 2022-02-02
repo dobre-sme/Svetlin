f8da9d69e715fcc3b34cba15657ac3800a72de61
Svetlin86 <93228248+Svetlin86@users.noreply.github.com>
2022-02-02 15:49:40 +0200

    Create Loops 9.java

---
+++ Loops 9.java
@@ -0,0 +1,75 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double numOfNum = scanner.nextDouble();
+
+        double oddMax = -100000000000000.0;
+        double oddMin = Double.MAX_VALUE;
+        double oddSum = 0.0;
+
+        double evenMax = -100000000000000.0;
+        double evenMin = Double.MAX_VALUE;
+        double evenSum = 0.0;
+
+
+        for (double i = 1; i <= numOfNum; i++) {
+            double num = scanner.nextDouble();
+
+            if (i % 2 == 0) {
+                evenSum += num;
+                if (num < evenMin) {
+                    evenMin = num;
+                }
+                else if (num > evenMax) {
+                    evenMax = num;
+                }
+                if (evenMin > evenMax) {
+                    evenMax = evenMin;
+                }
+            }
+            else {
+                oddSum += num;
+                if (num < oddMin) {
+                    oddMin = num;
+                }
+                else if (num > oddMax) {
+                    oddMax = num;
+                }
+                if (oddMin > oddMax) {
+                    oddMax = oddMin;
+                }
+            }
+        }
+
+        System.out.println("OddSum: " + oddSum);
+        if(oddMin != Double.MAX_VALUE){
+            System.out.println("OddMin: " + oddMin);
+        }else{
+            System.out.println("OddMin: No");
+        }
+        if(oddMax != -100000000000000.0){
+            System.out.println("OddMax: " + oddMax);
+        }else{
+            System.out.println("OddMax: No");
+        }
+
+
+        System.out.println("EvenSum: " + evenSum);
+        if(evenMin != Double.MAX_VALUE){
+            System.out.println("EvenMin: " + evenMin);
+        }else{
+            System.out.println("EvenMin: No");
+        }
+        if(evenMax != -100000000000000.0){
+            System.out.println("EvenMax: " + evenMax);
+        }else{
+            System.out.println("EvenMax: No");
+        }
+    }
+}
