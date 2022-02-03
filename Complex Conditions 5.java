a3c50707e09263b2db0149cbbeef5b6756ed1511
Svetlin86 <93228248+Svetlin86@users.noreply.github.com>
2022-02-03 17:10:39 +0200

    Create Complex Conditions 5.java

---
+++ Complex Conditions 5.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner =new Scanner(System.in);
+
+        double x1 = scanner.nextDouble();
+        double y1 = scanner.nextDouble();
+        double x2 = scanner.nextDouble();
+        double y2 = scanner.nextDouble();
+        double x = scanner.nextDouble();
+        double y = scanner.nextDouble();
+
+        if((x==x1 || x==x2 && y >= y1 && y <= y2)||(y==y1 || y==y2 && x >= x1 && x <= x2)){
+            System.out.println("Border");
+        }
+        else if(x >= x1 && x <= x2 && y >= y1 && y <= y2){
+            System.out.println("Inside");
+        }else {
+            System.out.println("Outside");
+        }
+
+    }
+}
