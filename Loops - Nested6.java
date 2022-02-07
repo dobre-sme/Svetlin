2ca5ae1f4dbbb227d8b0631e1ee8a232f4bc990c
Svetlin86 <93228248+Svetlin86@users.noreply.github.com>
2022-02-07 02:01:18 +0200

    Create Loops - Nested6.java

---
+++ Loops - Nested6.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+       while (b != 0){
+           int lastB = b;
+           b = a%b;
+           a=lastB;
+       }
+        System.out.println(a);
+    }
+}
