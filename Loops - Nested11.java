5816f5679393429c54c4fb385617130698f59a0e
Svetlin86 <93228248+Svetlin86@users.noreply.github.com>
2022-02-10 19:25:08 +0200

    Create Loops - Nested11.java

---
+++ Loops - Nested11.java
@@ -0,0 +1,28 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println(" Input : ");
+        int n = scanner.nextInt();
+
+        int n1=1;
+        int n2=1;
+        int n3=0;
+
+        if (n<2){
+            System.out.println("1");
+        }
+
+        for (int i = 2; i <= n; i++) {
+            n3=n1+n2;
+            n1=n2;
+            n2=n3;
+        }
+        System.out.println(n3);
+    }
+}
