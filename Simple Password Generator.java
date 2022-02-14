76197ba3d5d02e5b34e91eb7e4cd32f1625840f3
Svetlin86 <93228248+Svetlin86@users.noreply.github.com>
2022-02-14 13:37:14 +0200

    Create Simple Password Generator.java

---
+++ Simple Password Generator.java
@@ -0,0 +1,43 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        int l = scanner.nextInt(); 
+
+        for (int i = 1; i <= n; i++) {
+
+            for (int j = 1; j <= n; j++) {
+
+                char endk = (char) ('a' + l);
+
+                for (char k = 'a'; k < endk; k++) {
+
+                    char endm = (char) ('a' + l);
+
+                    for (char m = 'a'; m < endm; m++) {
+
+                        for (int o = 1; o <= n; o++) {
+
+                            if (o > i && o > j) {
+
+                                System.out.print(i+""+j+""+k+""+m+""+o+" ");
+                            }
+
+                        }
+
+                    }
+
+                }
+
+            }
+            System.out.println();
+        }
+
+    }
+}
