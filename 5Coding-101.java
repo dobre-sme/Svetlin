17af2db749eceaa94ebc0b2c7edcc1cbf92cea77
Svetlin86 <93228248+Svetlin86@users.noreply.github.com>
2022-02-15 21:33:31 +0200

    Update 5Coding-101.java

--- 5Coding-101.java
+++ 5Coding-101.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import java.time.LocalDate;
 import java.util.Scanner;
 
 public class Main {
@@ -7,35 +8,10 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String d = scanner.nextLine();
-        String m = scanner.nextLine();
+        int d = Integer.parseInt(scanner.nextLine());
+        int m = Integer.parseInt(scanner.nextLine());
 
-        int dayCount = Integer.parseInt(d);
-        int monthCount = Integer.parseInt(m);
-
-        int months = monthCount % 12;
-
-        if(m.equals("04")||m.equals("06")||m.equals("09")||m.equals("11")){
-            if(dayCount<25){
-                 dayCount=(dayCount-30)+5;
-            }else{
-                months++;
-            }
-        }if(m.equals("02")){
-            if(dayCount<23){
-                dayCount=dayCount+5;
-            }else{
-                dayCount=(dayCount-28)+5;
-                months++;
-            }
-        }else{
-            if(dayCount<26){
-                dayCount=dayCount+5;
-            }else{
-                dayCount=(dayCount-31)+5;
-                months++;
-            }
-        }
-        System.out.printf(dayCount + "." + "%02d", months);
+        LocalDate date = LocalDate.of(2020,d,m);
+        System.out.println(date.plusDays(5));
     }
 }
