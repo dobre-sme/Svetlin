e2b459bfaa9b6b4761d2afbbf05901d36a66503a
Svetlin86 <93228248+Svetlin86@users.noreply.github.com>
2022-02-15 22:33:23 +0200

    Create 7Coding-101.java

---
+++ 7Coding-101.java
@@ -0,0 +1,32 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+
+        int sum1 = 0;
+        int sum2 = 0;
+        int sum3 = 0;
+
+        for (int i = 0; i < n; i++) {
+            int a = scanner.nextInt();
+
+                if (i % 3 == 0) {
+                    sum1 += a;
+                } else if (i % 3 == 1) {
+                    sum2 += a;
+                } else if (i % 3 == 2) {
+                    sum3 += a;
+                }
+
+        }
+        System.out.println(sum1);
+        System.out.println(sum2);
+        System.out.println(sum3);
+    }
+}
