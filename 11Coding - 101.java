57ec203c965f2c15e0639c04ea210491afa707c1
Svetlin86 <93228248+Svetlin86@users.noreply.github.com>
2022-02-16 22:39:23 +0200

    Create 11Coding - 101.java

---
+++ 11Coding - 101.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt(), b = scanner.nextInt(),count = 0;
+
+        for (int i = a; i <=b ; i++) {
+            for (int j = i+1; j <=b ; j++) {
+                for (int k = j+1; k <=b ; k++) {
+                    for (int l = k+1; l <=b ; l++) {
+                        System.out.printf("%d %d %d %d %n",i,j,k,l);
+                        count++;
+
+                    }
+                }
+            }
+        }
+        if(count==0){
+            System.out.println("No");
+        }
+    }
+}
