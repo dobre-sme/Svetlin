a184d8286febc287b9b6699a705bc982e6859a8f
Svetlin86 <93228248+Svetlin86@users.noreply.github.com>
2022-02-16 12:54:14 +0200

    Update 3Coding-101.java

--- 3Coding-101.java
+++ 3Coding-101.java
@@ -1,5 +1,4 @@
 package com.company;
-
 import java.util.Scanner;
 
 public class Main {
@@ -10,14 +9,13 @@ public class Main {
         int first = scanner.nextInt(),second = scanner.nextInt(),point = scanner.nextInt(),
                 ds = Math.min(first, second), db = Math.max(first, second),
                 distance1 = Math.abs(point - ds), distance2 = Math.abs(point - db);
-        
-        boolean in = (first < point && point < second) || (first > point && point > second);
        
+        boolean in = (first < point && point < second) || (first > point && point > second);
+        
         if (in) {
-            System.out.println("in");
+            System.out.print("in"+"\n"+Math.min(distance1, distance2));
         } else {
-            System.out.println("out");
+            System.out.print("out"+"\n"+Math.min(distance1, distance2));
         }
-        System.out.println(Math.min(distance1, distance2));
     }
 }
