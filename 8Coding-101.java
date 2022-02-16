6a18591c17fde6909d31c47569647bcaedf40b2c
Svetlin86 <93228248+Svetlin86@users.noreply.github.com>
2022-02-16 10:50:21 +0200

    Create 8Coding-101.java

---
+++ 8Coding-101.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        int count = 0;
+        int na= 0;
+
+        for (int i = 0; i < n; i++) {
+            int a = scanner.nextInt();
+            if(a>na){
+                count++;
+            }
+            na = a;
+        }
+        System.out.println(count);
+    }
+}
