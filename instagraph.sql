a47916b555c9bb5c9d3f175238b6057ee61b3b93
Svetlin86 <93228248+Svetlin86@users.noreply.github.com>
2022-03-07 00:36:45 +0200

    Update instagraph.sql

--- instagraph.sql
+++ instagraph.sql
@@ -359,7 +359,9 @@ INNER JOIN users as u
 WHERE u.id = c.user_id
 ORDER BY c.id DESC;
 
-SELECT id,username,profile_picture_id FROM users AS u
+SELECT u.id,username,p.size FROM users AS u
+INNER JOIN pictures AS p
+ON p.id = u.profile_picture_id
 WHERE u.profile_picture_id 
 IN (SELECT inneru.profile_picture_id 
 FROM users AS inneru 
